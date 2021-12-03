1adada64b771308163c503d9874a1f224938863c
seasonalcodemonkey <seasonalcodemonkey@mail.com>
2021-12-03 00:08:08 +0000

    Day 2 part 2 challenge completed

--- day-2/challenge.py
+++ day-2/challenge.py
@@ -30,10 +30,32 @@ def calculate_position(commands):
 
     return (horizontal, depth)
 
+def calculate_position_aim(commands):    
+    horizontal = 0
+    depth = 0 
+    aim = 0
+
+    for command in commands:
+        part = command.split(" ")
+        
+        if len(part) == 2:
+            if part[0] == "forward":
+                horizontal += int(part[1])
+                depth += aim * int(part[1])
+            elif part[0] == "up":
+                aim -= int(part[1])
+            elif part[0] == "down":
+                aim += int(part[1])
+
+    return (horizontal, depth)
+
 if __name__ == "__main__":
     filename = "input"
     commands = load_input(filename)
 
     position = calculate_position(commands)
+    print("%s multiplied is %i" % (position, position[0] * position[1]))
+
+    position = calculate_position_aim(commands)
     print("%s multiplied is %i" % (position, position[0] * position[1]))
 
